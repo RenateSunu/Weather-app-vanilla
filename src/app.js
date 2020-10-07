677dd1e204eb6d052ded07d674108ce45a07cb01
RenateSunu <69839806+RenateSunu@users.noreply.github.com>
2020-10-07 22:36:30 +0100

    api function inputted

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-let apiKey = "936b1d5cb22c042e05ed5fbf928ca8b9";
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "0be12eeb3c9b6e4578dc00cdd6052381";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
